b85e2117aed294ad1297f7cf772c98118245bb80
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-11 22:34:38 +0500

    A2 task1 complete

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+
+if(B*B==A)
+{
+    Console.WriteLine("Да, B является квадратом A");
+}
+
+else
+{
+    Console.WriteLine("Нет, B не является квадратом A");
+}
+
