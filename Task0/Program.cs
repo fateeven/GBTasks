1c7e4f7e5459471a3d6c2443758b160e13234604
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-11 20:05:40 +0500

    A1 task0 complete

---
+++ Task0/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа равен: {sqr}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа, найденый 2-ым способом, равен: {sqr1}");
