e25defc4431943e65268a6f2604f1c290f189374
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-14 00:16:11 +0500

    A9 task8_dz4 complete

---
+++ Task8_dz4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int S = 0;
+while(i<N)
+{
+    if(i%2==0)
+    {
+        S = i;
+        Console.Write($"{S}, ");
+    }
+
+    i++;
+}
