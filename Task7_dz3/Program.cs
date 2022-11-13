c92408773ced2bbe9b05088f728c36af3e03a83c
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-13 23:43:20 +0500

    A8 task7_dz3 complete

---
+++ Task7_dz3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N%2==0)
+{
+    Console.WriteLine($"Число {N} делится на 2 без остатка");
+}
+else
+{
+    Console.WriteLine($"Число {N} не делится на 2 без остатка"); 
+}
