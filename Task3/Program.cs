fba34618c9e1511d2712fde9ee85e2ce146b775f
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-13 21:57:20 +0500

    A4 task3 complete

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int S = -N;
+while (S<=N)
+{
+    Console.Write($"{S} ");
+    S++;
+}
