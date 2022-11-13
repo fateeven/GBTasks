cab10aade2c175d997c9f77f6b54755e31d5d05a
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-13 22:41:50 +0500

    A6 task5_dz1 complete

---
+++ Task5_dz1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int max = A;
+if(max<B)
+{
+    max = B;
+}
+Console.WriteLine($"max= {max}");
