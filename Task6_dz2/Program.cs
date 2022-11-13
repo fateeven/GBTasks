708d74ddf07d7875108fe8e857a9035ac2e18e15
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-13 23:08:15 +0500

    A7 task6_dz2 complete

---
+++ Task6_dz2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int C = int.Parse(Console.ReadLine());
+int max = A;
+if (max < B)
+{
+    max=B;
+}
+if (max < C)
+{
+    max=C;
+}
+Console.WriteLine($"Максимальное из введенных чисел:-> {max} ");
