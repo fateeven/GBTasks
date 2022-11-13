29edb2ac889f7dfd6adecf31c7f816f12991a4e1
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-13 22:22:19 +0500

    A5 task4 complete

---
+++ Task4/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+int sqr = N%10;
+Console.WriteLine($"Последняя цифра: {sqr}");
