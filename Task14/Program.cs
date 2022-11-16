14964bf02b99f4dfad300ab55b5ddce64c6e14f2
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-16 17:13:34 +0500

    1

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int N = int.Parse(ReadLine());
+if((N % 7 ==0)||(N % 23 == 0)) // || можно заменить на &&
+{
+    WriteLine($"{N} -> Да");
+}
+else
+{
+    WriteLine($"{N} -> Нет");
+}
