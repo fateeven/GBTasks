32df5243ed72ebb0fac80529b65e57ff1b49456c
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-16 17:50:10 +0500

    A17 task16 complete

---
+++ Task16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите A: ");
+int A = int.Parse(ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(ReadLine());
+
+if(B*B==A)  //используем математический метож
+{
+    WriteLine($" {A} является квадратом {B}");
+}
+else
+{
+    if(Math.Pow(A, 2)==B) //используем функциию, заменяет(A*A==B)
+    {
+        WriteLine($" {B} является квадратом {A}");
+    }
+    else
+    {
+        WriteLine("Не являеться");
+    }
+    
+}
