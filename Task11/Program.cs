b3898ba33d1be7668eefd7285bbd79cff454e371
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-16 16:19:09 +0500

    A13 task11 complete

---
+++ Task11/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using static System.Console;
+Clear();
+int number = new Random().Next(100, 1000);
+int a1 = number / 100;
+int a2 = number % 10;
+WriteLine($"{number}->{a1}{a2}");
+WriteLine($"{number}->{a1*10+a2}"); //Решение преподователя
+WriteLine($"{number}->{(number / 100) * 10 + (number % 10)}");  //Можно обойтись без переменных a1 и a2
