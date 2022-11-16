f00ca687e7454b738030e1bcaf10475f9ca478c5
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-16 15:52:03 +0500

    A12 task9 complete

---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console; //данная команда позволяет далее в коде не прописывать Console
+Clear();
+int number = new Random().Next(10, 100); // Второе значение берем на 1 больше(учитывыем нумерацию индекса с 0)
+int a1 = number / 10;
+int a2 = number % 10;
+if(a1 > a2)
+{
+    WriteLine($"{number}->{a1}");
+}
+else
+{
+    WriteLine($"{number}->{a2}");
+}
+
