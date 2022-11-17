05c69824e144f9a98e241ac5c9d102871a9b48e5
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-17 16:34:59 +0500

    A21 task15 fixed

---
+++ Task15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите число дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine($"{1} -> нет");
+}
+if (number == 2)
+{
+    Console.WriteLine($"{2} -> нет");
+}
+if (number == 3)
+{
+    Console.WriteLine($"{3} -> нет");
+}
+if (number == 4)
+{
+    Console.WriteLine($"{4} -> нет");
+}
+if (number == 5)
+{
+    Console.WriteLine($"{5} -> нет");
+}
+if (number == 6)
+{
+    Console.WriteLine($"{6} -> да");
+}
+if (number == 7)
+{
+    Console.WriteLine($"{7} -> Да");
+}
+if (number>7||number<1)
+{
+    Console.WriteLine("Ошибка ввода");
+}
