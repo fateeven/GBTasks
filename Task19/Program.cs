ed6f6a26d35585dac2f828bcd52e7884db99ca79
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-18 16:50:44 +0500

    A26 task19 complete

---
+++ Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите пятизначное число: ");
+int N = int.Parse(ReadLine());
+if((N>9999)&&(N<100000))
+{
+    int x = N/10000;
+    int y = N%10;
+    int z = (N/1000)%10;
+    int f = (N%100)/10;
+    int u = (N/100)%10;
+
+    if((x==y)&&(z==f))
+    {
+        Write($"{N} -> Да, является палиндромом");
+        Write($" -> {x}={y}, {u}={u}, {z}={f}");
+    }
+    else
+    {
+        Write($"{N} -> Нет, не является палиндромом");
+    }
+}
+else
+{
+    Write("Ошибка ввода");
+}
