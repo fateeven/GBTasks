5b0cb9a9f212675d07eb8b9b3c3abde8479f86ae
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-22 22:12:33 +0500

    A34 task16 fixed+task summ_epsilon

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -18,7 +18,27 @@ else
     }
     else
     {
-        WriteLine("Не являеться");
+        WriteLine("Не является");
     }
     
 }
+
+//Дополнительная задача!
+//16.1
+//Вычислить сумму ряда с указанной точностью (e=0,001)
+//1+1/2+1/3+1/4...1/бесконечность
+//Если последняя точность, последнего действия меньше чем e, то завершаем цикл с помощью break;
+
+double summ = 0;
+double lastOperation = 0;
+const double eps = 0.001; //обяъвляем переменную эпсилон
+int divider =1;
+
+do  //тоже самое что while, но сперва выполняеться тело метода, а после while
+{
+    lastOperation = (double)1 / divider;     // присвоили еденице тип данных double, так как вычесляем вещественные числа
+    System.Console.WriteLine(lastOperation); // double можно еще написать как 1.0 Пример: 1.0 / 2 = 0.5
+    summ+=lastOperation;
+    divider++;                 
+}while (lastOperation > eps);
+System.Console.WriteLine($"answer = {summ}");
