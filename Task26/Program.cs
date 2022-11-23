21c2a412390cfd6fa335998d22ba534ea96757ba
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-23 17:57:51 +0500

    A38 task26 complete

---
+++ Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+Clear();
+//основной код программы
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+int summa = GetCountNumbers(N);
+WriteLine($"{N} -> Кол-во цифр: {summa}");
+
+
+
+int GetCountNumbers(int number)
+{
+    int count = 0; // переменная которая будет возвращаться
+    while(number>0)
+    {
+        number/=10;   // number = number / 10;
+        count++;     // count = count + 1;
+    }
+    return count;  //для того чтобы наша функция что-то вернула, пишем команду return и переменную count, которую будем возвращать
+}
