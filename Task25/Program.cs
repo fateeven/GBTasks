ad2dfc4c3aef3ee18b1159ea49d960616a2486b8
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-24 23:33:15 +0500

    A42 task25 complete

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число A: ");
+int A = int.Parse(ReadLine());
+Write("Введите число B: ");
+int B = int.Parse(ReadLine());
+int Pow = GetPowNumbers(A, B);
+
+if (A < 0 || B < 0)
+    WriteLine("Введите значения A и B => 0");
+else
+    WriteLine($"{A} в степени {B} = {Pow}");
+
+//метод to the power
+int GetPowNumbers(int x, int y)
+{
+    if (y == 0)
+    {
+        return 1;
+    }
+    int result = x;
+    for (int i = 2; i <= y; i++) // Важно! i=2, так как любое х - уже является первой степенью!
+    {                        // Важно! Если присвоить i=0 или i=1, будет неверный результат, так произойдет лишнее умножение
+        result*=x;
+    }
+    return result;
+}
