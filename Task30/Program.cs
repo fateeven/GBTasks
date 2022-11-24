8e98bbc9ab9abe64017491d055ab2c0df4371ba9
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-24 21:52:22 +0500

    A41 task30 complete

---
+++ Task30/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine($"[{String.Join(", ", GetBinaryArray(8))}]");
+
+
+
+//метод
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i=0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
