3593c518943a8d16380bbf6fccaf1c7c759059c2
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-25 15:32:38 +0500

    A44 task29 complete

---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите нужную длинну массива(кол-во чисел): ");
+int A = int.Parse(ReadLine());
+Write("Введите минимальное значение массива: ");
+int B = int.Parse(ReadLine());
+Write("Введите максимальное значение массива: ");
+int C = int.Parse(ReadLine());
+
+WriteLine($"[{String.Join(", ", GetRandomArray(A, B, C))}]");
+
+
+
+//метод создания рандом
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+}
