c603e7027e2e79ee0df5a47984f272e4f316ced6
Evgeny Fateev <fateyev_84@mail.ru>
2022-11-28 18:49:35 +0500

    A52 task32 fixed task37 start

--- Task32/Program.cs
+++ Task32/Program.cs
@@ -7,23 +7,15 @@ int[] array = GetArray(8, -10, 10);
 string G = String.Join(", ", array);                     
 InverseArray(array);
 string I = String.Join(", ", array);
-Write($"[{G}] --> [{I}]");                //строка нужна только для оформления ответа в []  
+Write($"[{G}] --> [{I}]");   //строка нужна только для офрмления ответа в []  
 
 
-//метод gjbcrf чисел  в массиве, без возврата значения
-void InverseArray(int[] inArray, int find)
+//метод инверсии чисел  в массиве, без возврата значения
+void InverseArray(int[] inArray)
 {
-    
     for (int i = 0; i < inArray.Length; i++) // добавили к переменной array свойство Length которое вернет нам кол-во элиментов в массиве
     {
-        if (inArray[i] == find)
-        {
-            Writeline(inArray);
-        }
-        else
-        {
-            Writeline("Искомого числа в масиве нет");
-        } 
+        inArray[i] *= -1; //значение каждого индекса в массиве умножается на -1.
     }
 }
 
@@ -31,7 +23,7 @@ void InverseArray(int[] inArray, int find)
 int[] GetArray(int size, int min, int max)
 {
     int[] result = new int[size];
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)  //// Важно! не требуется .Length
     {
         result[i] = new Random().Next(min, max + 1);
     }
---
+++ Task37/Program.cs
@@ -0,0 +1,23 @@
+﻿//Найдите произведение пар чисел в одномерном массиве.
+//Парой считаем - первой и последний элемент массива.
+//Парой считаем - второй и предпоследний элемент массива и т.д.
+//Результат выводим как новый массив из вычесленых произведений пар.
+using System;
+using static System.Console;
+
+Clear();
+
+
+
+
+//метод создания массива из строки, метод позволит всю строчку разбить и преобразавать в числовой массив.
+int[] GetArrayFromString(string stringArray)
+{                                                                                     //StringSplitOptions.RemoveEmptyEntries - удаляет из массива двойные пробелы
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries); //данный оператор разбивает на заданные элементы массив.
+    int[] result = new int[numS.Length];
+    for (int i = 0; i < result.Length; i++) //необходимо добавить служебное слово .Length
+    {
+        result[i] = int.Parse(numS[i]);  //данным действием мы набираем значения в массив result
+    }
+    return result;
+}
