669ee229b72046082a9a977d1b2734792d41ae13
Evgeny Fateev <fateyev_84@mail.ru>
2022-12-02 00:57:05 +0500

    A64 task46 complete

---
+++ Task46/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите кол-во строк и столбцов двумерного массива через пробел: ");
+string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Write("Введите минимальное и максимальное значения двумерного массива через пробел: ");
+string[] values = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int[,] array = GetATwoDimensionalArray(int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(values[0]), int.Parse(values[1]));
+PrintATwoDimensionalArrey(array);
+
+
+//Метод формирования двумерного массива
+int[,] GetATwoDimensionalArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    
+    for (int i = 0; i < result.GetLength(0); i++)                  // цикл для rows
+    {
+        for (int j = 0; j < result.GetLength(1); j++)              // цикл для columns
+        {
+            result[i,j] = new Random().Next(minValue, maxValue+1); // рандомно запалняем массив с учетом минимального и максимального значения
+        }
+    }
+    return result;
+}
+
+//Метод Void печати двумерного массива 
+void PrintATwoDimensionalArrey(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");                     // не забываем пробел для столбцов
+        }
+        WriteLine();                                        // не забываем пробел для строк             
+    }
+}
