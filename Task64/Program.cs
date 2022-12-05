1a3023656d88e37877c2b2e5179380e97e8a568e
Evgeny Fateev <fateyev_84@mail.ru>
2022-12-05 16:50:43 +0500

    A88 task64 complete

---
+++ Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите значение N: ");
+int n = int.Parse(ReadLine());
+Write("Введите значение M: ");
+int m = int.Parse(ReadLine());
+
+if(m>n)
+{
+    WriteLine(StringPrintNumbers(n, m));
+}
+else
+{
+    WriteLine(StringPrintNumbers(m, n));
+}
+
+//метод вернет строку состоящую из чисел от N до 1 в обратном порядке
+string StringPrintNumbers(int start, int end)
+{
+    if (start == end) return end.ToString();
+    return end + " " + StringPrintNumbers(start, end - 1);
+}
---
+++ Task69/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа на вход принимает два числа А и B, и возводит число А в целую степень B
+//с помощью рекурсии
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите значение A: ");
+int A = int.Parse(ReadLine());
+Write("Введите значение B: ");
+int B = int.Parse(ReadLine());
+WriteLine($"{A} в степени {B} = {Pow(A,B)};");
+
+//метод возведения в степень с помощью рекусии
+int Pow(int number, int degree)
+{
+    if(degree==0) return 1;
+    if(degree==1) return number;
+    return(number*Pow(number, degree-1));
+}
