7121a8bc2b6fa86741cb540d0a6e14a8d4763175
Evgeny Fateev <fateyev_84@mail.ru>
2022-12-05 17:06:34 +0500

    A87 task67 complete

---
+++ Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает на вход число и возвращает сумму его цифр
+//456 -> 15
+//12 -> 3
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите значение N: ");
+int n = int.Parse(ReadLine());
+WriteLine($"Cумма цифр числа = {Sum(n)}");
+
+//Метод возвращает сумму цифр числа
+int Sum(int number)
+{
+    if(number==0) return 0;  //return 0 позволит завершить нашу рекусию
+    return(number%10+Sum(number/10));
+}
