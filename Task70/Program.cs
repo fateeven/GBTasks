2b066cc57ea5953a401f33c1618efb7c7d7c5700
Evgeny Fateev <fateyev_84@mail.ru>
2022-12-06 10:44:53 +0500

    A91 task70 complete

---
+++ Task70/Program.cs
@@ -0,0 +1,24 @@
+﻿//Программа на вход принимает два числа и выдает первые N чисел, для которых
+//каждое следующее равно сумме предыдущих
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите первое число: ");
+int a1 = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int a2 = int.Parse(ReadLine());
+Write("Введите длину ряда: ");
+int size = int.Parse(ReadLine());
+Write($"{a1} {a2} ");
+WriteLine(PrintNumber(a1,a2,size));
+
+
+
+//метод возращения массива
+string PrintNumber(int start, int end, int length)
+{
+    if(length==0) return "";
+    return (start+end+ "  " +PrintNumber(end, start+end, length-1));
+}
