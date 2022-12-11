4de1674aabaf8fcc573a65710017edeac95b1a95
Evgeny Fateev <fateyev_84@mail.ru>
2022-12-11 23:57:13 +0500

    A99 add fixed task and add README 4

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
+﻿//Программа, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет является ли этот день выходным
+using System;
 Console.Clear();
+
 Console.Write("Введите число дня недели: ");
 int number = int.Parse(Console.ReadLine());
-if (number>7||number<1)
+
+if (number > 7 || number < 1)
 {
     Console.WriteLine("Ошибка ввода");
     return;
 }
 
-
-if (number > 5 )
+if (number > 5)
 {
     Console.WriteLine($" - > Да");
 }
--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿//Програма, которая принимает на вход два числа и проверяет,
+//является ли одно число квадратом другого
+using System;
 using static System.Console;
+
 Clear();
 Write("Введите A: ");
 int A = int.Parse(ReadLine());
-Console.Write("Введите B: ");
+Write("Введите B: ");
 int B = int.Parse(ReadLine());
 
-if(B*B==A)  //используем математический метож
+if (B * B == A)  //используем математический метод
 {
     WriteLine($" {A} является квадратом {B}");
 }
 else
 {
-    if(Math.Pow(A, 2)==B) //используем функциию, заменяет(A*A==B)
+    if (Math.Pow(A, 2) == B) //используем функциию, заменяет(A*A==B)
     {
         WriteLine($" {B} является квадратом {A}");
     }
@@ -20,25 +23,24 @@ else
     {
         WriteLine("Не является");
     }
-    
 }
 
 //Дополнительная задача!
 //16.1
 //Вычислить сумму ряда с указанной точностью (e=0,001)
-//1+1/2+1/3+1/4...1/бесконечность
+//1+1/2+1/3+1/4...и т.д
 //Если последняя точность, последнего действия меньше чем e, то завершаем цикл с помощью break;
 
 double summ = 0;
 double lastOperation = 0;
 const double eps = 0.001; //обяъвляем переменную эпсилон
-int divider =1;
+int divider = 1;
 
 do  //тоже самое что while, но сперва выполняеться тело метода, а после while
 {
     lastOperation = (double)1 / divider;     // присвоили еденице тип данных double, так как вычесляем вещественные числа
     System.Console.WriteLine(lastOperation); // double можно еще написать как 1.0 Пример: 1.0 / 2 = 0.5
-    summ+=lastOperation;
-    divider++;                 
-}while (lastOperation > eps);
+    summ += lastOperation;
+    divider++;
+} while (lastOperation > eps);
 System.Console.WriteLine($"answer = {summ}");
